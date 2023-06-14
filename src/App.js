606cba80f15dba17d4a8039f648f90ab68a6419e
ialokranj <75290267+ialokranj@users.noreply.github.com>
2023-06-14 14:42:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Maincontent from './components/Maincontent'
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='section-elements'>
+      <Header />
+      <Maincontent name = "Profile Summary"/><hr></hr>
+      <Maincontent name = "Academic and Cocurricular Achievements"/><hr></hr>
+      <Maincontent name = "Summer Internship Experience"/><hr></hr>
+      <Maincontent name = "Work Experience"/><hr></hr>
+      <Maincontent name = "Projects"/><hr></hr>
+      <Maincontent name = "Certifications"/><hr></hr>
+      <Maincontent name = "Leadership Positions"/><hr></hr>
+      <Maincontent name = "Extracurricular"/><hr></hr>
+      <Maincontent name = "Education"/>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
